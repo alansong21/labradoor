3a7e3c9bb9c43ce5ccc30f57caccd3c86a9b53ca
alansong21 <alansong21@gmail.com>
2025-10-15 22:04:57 -0700

    basic file structure

--- app/api/server.js
+++ app/api/server.js
@@ -1,10 +1,10 @@
 import http from 'http';
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer((req, res) => {
-    res.write('Shalom');
-    res.end();
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Server Error'}));
 });
 
 server.listen(PORT, () => {
